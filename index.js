3f66debdd5d1e65bae965cfe8c74264bc1eeb0c5
Matthew Parker <matt.parker1007@gmail.com>
2021-04-12 18:57:39 -0500

    created questions array and init func

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
 var inquirer = requires('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Project title: '
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Project description: '
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Installation instructions: '
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Usage information: '
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Contribution guidelines: '
+  },
+  {
+    type: 'input',
+    name: 'testInstr',
+    message: 'Test instructions: '
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'GitHub username: '
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Installation instructions: '
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Email address: '
+  }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  // not sure if this goes here...
+  inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, ' '));
+  })
+}
 
 // Function call to initialize app
 init();
